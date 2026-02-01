02890165e77e039e46b7c700ab29e78e5cd0d942
iwillmedia <willrogersco@gmail.com>
2026-02-01 11:04:55 +0930

    Add LinkedIn profile link

--- App.tsx
+++ App.tsx
@@ -302,7 +302,7 @@ const App: React.FC = () => {
           </div>
           <div className="mt-10 flex flex-wrap items-center gap-4">
             {[
-              { label: 'LinkedIn', href: '#' },
+              { label: 'LinkedIn', href: 'https://www.linkedin.com/in/iwillmediaco/' },
               { label: 'Instagram', href: '#' },
               { label: 'X', href: '#' },
               { label: 'Facebook', href: 'https://www.facebook.com/iwillmedia.co/' },
